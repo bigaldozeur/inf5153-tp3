5caef26ea39123ffc27e79df687573ddde142805
akimtouat <akim.l.touat@gmail.com>
2018-04-19 23:51:40 -0400

    Nom branche
    Changer nom branche

--- tp3/src/main/java/edu/uqam/inf5153/tp3/domaine/Dossier.java
+++ tp3/src/main/java/edu/uqam/inf5153/tp3/domaine/Dossier.java
@@ -1,12 +1,67 @@
 package edu.uqam.inf5153.tp3.domaine;
 
+import edu.uqam.inf5153.tp3.application.ControleurDeGuiApp;
+
 public class Dossier {
 	
 	
 	private String maladie = new String();
 	private String medecin = new String();
 	
-	public Dossier(){}
+	public Dossier(String nuAssurance) {
+					
+	}
+	
+	public void setNom() {
+		
+	}
+	
+	public void setPrenom() {
+		
+	}
+	
+	public void setGenre() {
+		
+	}
+	
+	public void setParents() {
+		
+	}
+	
+	public void setVilleDeNaissance() {
+		
+	}
+	
+	public void setCoordonnees() {
+		
+	}
+	
+	public String getNom() {
+		
+	}
+	
+	public String getPrenom() {
+		
+	}
+	
+	public String getGenre() {
+		
+	}
+	
+	public String getParents() {
+		
+	}
+	
+	public String getVilleDeNaissance() {
+		
+	}
+	
+	public String[] getCoordonnees() {
+		
+	}
+	public String getNoAssuranceMaladie() {
+		
+	}
 	
 	public String getMaladie() {
 		return maladie;
